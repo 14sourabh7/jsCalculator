660eade369bc5cc6178351f6d8dbbd901e209035
14sourabh7 <sourabhsingh@cedcoss.com>
2022-02-19 10:23:01 +0530

    final submit

--- src/script.js
+++ src/script.js
@@ -1,42 +1,61 @@
 var result = 0;
 var operator = "+";
+var num = "";
+var disp = "n";
 
-function numberInput(num) {
-  switch (operator) {
-    case "+":
-      result += Number(num);
-      break;
-    case "-":
-      result -= num;
-      break;
-    case "/":
-      result /= num;
-      break;
-    case "*":
-      result *= num;
-      break;
+// main input and calculation function
+function numberInput(numInp) {
+  if (isNaN(numInp) && numInp != ".") {
+    switch (operator) {
+      case "+":
+        result += Number(num);
+        break;
+      case "-":
+        result -= num;
+        break;
+      case "/":
+        result /= num;
+        break;
+      case "*":
+        result *= num;
+        break;
+      default:
+        display(result);
+    }
+    operator = numInp;
+    console.log(result);
+    num = "";
+    disp = "r";
+    display(result);
+  } else {
+    num += numInp;
+    display(num);
+    disp = "n";
   }
-
-  display(result);
-}
-function operatorInput(opr) {
-  operator = opr;
 }
 
+// display function
 function display(num) {
   document.getElementById("input").innerHTML = num;
 }
+
+// function for AC
 function setzero() {
   result = 0;
+  num = "";
   operator = "+";
   display(result);
 }
+
+// function to delete single digit
 function delDigit() {
-  r = result % 10;
-  result -= r;
-  result /= 10;
-  display(result);
-}
-function Calculate() {
-  display(result);
+  if (disp == "r") {
+    r = result % 10;
+    result -= r;
+    result /= 10;
+    display(result);
+  } else {
+    num = num.slice(0, -1);
+    display(num);
+  }
 }
